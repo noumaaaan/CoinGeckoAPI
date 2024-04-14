87f5e44a898e6bffd6cf822acfe820da1b013817
Nouman Mehmood <nouman_7@live.co.uk>
2024-04-14 04:16:58 +0100

    Initial Commit

---
+++ CoinGecko/CoinGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoApp.swift
+//  CoinGecko
+//
+//  Created by Noumaan Mehmood on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinGecko/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinGecko
+//
+//  Created by Noumaan Mehmood on 14/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
